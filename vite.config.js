1857210c06730bad35a47aa01c9f53bd710c014c
Emile Dufoulon <emile.dufoulon@outlook.com>
2026-01-05 16:26:46 +0100

    modification vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
-  base: "/portfolio-emile/", // <-- ton nom de repo GitHub
+  base: "/", // <-- ton nom de repo GitHub
   plugins: [react()],
 });
